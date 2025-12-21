15976845f67c89b7fba69de4449bed00d8052916
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-12-21 22:28:13 +1100

    pass question mark?

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -11,6 +11,10 @@ SKIP_DOCKER="${SKIP_DOCKER:=false}"
 in_ci="false"
 
 function setup_db_container() {
+    if [[ "$SKIP_DOCKER" == "true" ]]; then
+        return
+    fi
+
     docker run \
         --env POSTGRES_USER="${SUPERUSER}" \
         --env POSTGRES_PASSWORD="${SUPERUSER_PWD}" \
@@ -47,7 +51,7 @@ fi
 
 # if the postgres container doesn't already exist and we are in CI as if we are not in CI we will
 # be connecting to supabase db and don't need to spin up a local container
-if [[ ! "$(docker ps -aq -f name=${CONTAINER_NAME})" ]] && [[ in_ci == "true" ]] && [[ SKIP_DOCKER == "false" ]]; then
+if [[ ! "$(docker ps -aq -f name=${CONTAINER_NAME})" ]] && [[ in_ci == "true" ]]; then
     setup_db_container
 fi
 
