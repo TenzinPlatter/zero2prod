818c4fe0514e55c7c3da0af8ba184b0b2515d3e7
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-12-22 12:07:45 +1100

    fix nextest install

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use anyhow::{Result, bail};
+use anyhow::{Result};
 
 use zero2prod::spawn_app;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt().with_env_filter(
-        tracing_subscriber::EnvFilter::from_default_env()
-            .add_directive(tracing::Level::INFO.into()),
-    ).init();
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive(tracing::Level::INFO.into()),
+        )
+        .init();
 
-    if let Ok(app) = spawn_app().await {
-        println!("Server running on {}", app.address);
-        Ok(app.handle.await??)
-    } else {
-        bail!("Failed to spawn application")
-    }
+    let app = spawn_app().await?;
+    println!("Server running on {}", app.address);
+    app.handle.await??;
+    Ok(())
 }
